3a222a2b44ac5fd205712f6c1a893ff6a5f1f7ad
Kurt Gehl <kgehl@apspayroll.com>
2022-08-05 07:08:34 -0500

    Add mail

---
+++ src/main/java/com/kurtg/gateway/config/Mail.java
@@ -0,0 +1,41 @@
+package com.kurtg.gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+public class Mail {
+    @Value("${spring.mail.host}")
+    private String host;
+    @Value("${spring.mail.port}")
+    private int port;
+    @Value("${spring.mail.username}")
+    private String userName;
+    @Value("${spring.mail.password}")
+    private String password;
+    @Value("${spring.mail.properties.mail.smtp.auth}")
+    private String useAuth;
+    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+    private String useTls;
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+
+        mailSender.setUsername(userName);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", useAuth);
+        props.put("mail.smtp.starttls.enable", useTls);
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
